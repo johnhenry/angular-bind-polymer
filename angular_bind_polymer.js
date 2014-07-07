e128479f9c4a8c342c5884e1a35cefcc7d6ac41d
David Klemenc <klemencdavid@gmail.com>
2014-07-07 12:00:41 +0200

    add: bindPolymerToggle
    You can now bind a toggle polymer element

--- angular_bind_polymer.js
+++ angular_bind_polymer.js
@@ -1,49 +1,4 @@
-angular.module('eee-c.angularBindPolymer', []).
-directive('bindPolymer', ["$q", "$timeout", function($q, $timeout) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var attrMap = {};
-            for (var prop in attrs.$attr) {
-                if (prop != 'bindPolymer') {
-                    var _attr = attrs.$attr[prop];
-                    var _match = element.attr(_attr).match(/\{\{\s*(\w+)\s*\}\}/);
-                    if (_match) {
-                        attrMap[_attr] = _match[1];
-                    }
-                }
-            }
-
-            // Always get updated Polymer element
-            function polymer() {
-                var all = document.querySelectorAll(element[0].nodeName);
-                for (var i = 0; i < all.length; i++) {
-                    if (all[i] == element[0]) return all[i];
-                    if (all[i].impl == element[0]) return all[i];
-                }
-            }
-
-            // When Polymer sees a change to the bound variable,
-            // $apply / $digest the changes here in Angular
-            var observer = new MutationObserver(function() {
-                scope.$apply();
-            });
-            observer.observe(polymer(), {
-                attributes: true
-            });
-            for (var _attr in attrMap) {
-                scope.$watch(
-                    function() {
-                        return element.attr(_attr);
-                    },
-                    function(value) {
-                        scope[attrMap[_attr]] = value;
-                    }
-                );
-            }
-        }
-    };
-}]).
+angular.module('gr.polymerBind', []).
 directive('bindPolymerInput', ['$parse', function($parse) {
     return {
         restrict: 'A',
@@ -53,6 +8,20 @@ directive('bindPolymerInput', ['$parse', function($parse) {
             });
             element[0].addEventListener('input', function() {
                 $parse(attrs.bindPolymerInput).assign(scope, element[0].inputValue);
+                scope.$apply();
+            });
+        }
+    };
+}]).
+directive('bindPolymerToggle', ['$parse', function($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            scope.$watch(attrs.bindPolymerToggle, function(value) {
+                element[0].ariaPressed = value;
+            });
+            element[0].addEventListener('change', function() {
+                $parse(attrs.bindPolymerToggle).assign(scope, element[0].checked);
                 scope.$apply();
             });
         }
